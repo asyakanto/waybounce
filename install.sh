73c87d76768885b31bed6f69ae85e4a373e9cd1b
asyakanto <ars9iy@gmail.com>
2026-05-11 16:37:00 +0300

    install fixes

--- install.sh
+++ install.sh
@@ -90,6 +90,10 @@ echo "# Waybounce binds" >> "$HYPR"
 add "bind = SUPER_ALT, D, exec, systemctl --user restart waybounce.service"
 add "bind = SUPER_SHIFT, D, exec, systemctl --user stop waybounce.service"
 
+hyprctl reload
+
+rm -rf "~/waybounce"
+
 # 7. Finish
 echo "=== Installation complete ==="
 echo "Start: Super Alt D"
